76c89e79594ee9cd4a563cf13f8975d020b12e52
Geraldo Andrade <geraldo.andrade@humanitec.com>
2019-01-02 13:13:10 -0200

    Fixed usage message

--- service_builder/templates/scripts/wait-for-it.sh
+++ service_builder/templates/scripts/wait-for-it.sh
@@ -4,8 +4,8 @@ set -e
 
 if [[ -z "$1" || -z "$2" ]]
 then
-    echo "tcp-port-wait - block until specified TCP port becomes available"
-    echo "Usage: tcp-port-wait HOST PORT"
+    echo "tcp-port-wait.sh - block until specified TCP port becomes available"
+    echo "Usage: tcp-port-wait.sh HOST PORT"
     exit 1
 fi
 echo Waiting for port $1:$2 to become available...
