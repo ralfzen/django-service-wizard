d0e986da1075fb2fe23eba4dbbf131f57a2a8911
Ralf Kostulski <639859+ralfzen@users.noreply.github.com>
2019-04-07 15:06:24 +0200

    Fix long loadings in local gunicorn

--- service_builder/templates/docker/docker-entrypoint-dev.sh
+++ service_builder/templates/docker/docker-entrypoint-dev.sh
@@ -12,4 +12,4 @@ echo $(date -u) "- Creating admin user"
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
 
 echo $(date -u) "- Running the server"
-gunicorn {{ name_project }}.wsgi --config {{ name_project }}/gunicorn_conf.py --reload
+gunicorn {{ name_project }}.wsgi --config {{ name_project }}/gunicorn_conf.py --workers 2 --timeout 120 --reload
